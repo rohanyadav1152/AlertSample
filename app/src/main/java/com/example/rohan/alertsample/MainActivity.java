5d1b459abf012590173852bc2097b7776a05122a
rohanyadav1152 <rohanyadav13396@gmail.com>
2018-07-22 10:55:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohan/alertsample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.rohan.alertsample;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle(R.string.alert_title);
+        myAlertBuilder.setMessage(R.string.alert_message);
+        myAlertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+        myAlertBuilder.setNegativeButton("Cancel", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    });
+
+                        myAlertBuilder.show();
+                    }
+    }
+
+
+
+
+
+
